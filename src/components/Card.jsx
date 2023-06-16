ced12f70bdcc002a3667563ef852dec337faf6c3
becs <angpohye@gmail.com>
2023-06-16 16:29:27 +0800

    set animation duration

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,9 +3,11 @@ import { motion } from "framer-motion";
 function Card( {toilet} ) {
     return (
         <motion.div 
+            Layout
             animate={ { opacity: 1 } }
             initial={ { opacity: 0 } }
             exit={ { opacity: 0 } }
+            transition={ { duration: 0.5 } }
             className="card">
                 <p className="tag">{toilet.location}</p>
                 <p className="tag">{toilet.address}</p>
