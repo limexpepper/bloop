39fe84f536f9dbf0b5f380524eaedd1417d1300c
becs <angpohye@gmail.com>
2023-07-17 16:53:37 +0800

    [wip] styling cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -10,10 +10,14 @@ function Card( {filteredItem} ) {
             exit={ { opacity: 0 } }
             transition={ { duration: 0.5 } }
             className="card">
-                <h2>{filteredItem.location}</h2>
-                <p>{filteredItem.address}</p>
-                <p>{filteredItem.rating}</p>
-                <p>{filteredItem.type}</p>
+                <div className="photo"></div>
+                <div className="rating"><p>{filteredItem.rating}</p></div>
+                <div className="review-count"><p>Based on xx reviews</p></div>
+                <div className="location"><h2>{filteredItem.location}</h2></div>
+                <div className="swiper-wrapper">
+                    <div className="swiper"></div>   
+                    <div className="swiper"></div>             
+                </div>
         </motion.div>
     )
 }
