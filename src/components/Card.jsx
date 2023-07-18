209165bada1b392a59615b05a4ab5fb3507dd45d
becs <angpohye@gmail.com>
2023-07-18 16:05:42 +0800

    configured swiperJS for swipe-able card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,25 +1,60 @@
 import { motion } from "framer-motion";
+import { Swiper, SwiperSlide } from "swiper/react"; // importing as per Swiper's documentation
+import { Navigation, Pagination, A11y } from "swiper/modules"; // importing as per Swiper's documentation
+//import 'swiper/swiper-bundle.min.css'; // see https://www.youtube.com/watch?v=KL_yIf5uiJo 13:54 to resolve swiper/css filepath issue
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
 
-function Card( {filteredItem} ) {
-    console.log(filteredItem);
-    return (
-        <motion.div 
-            Layout
-            animate={ { opacity: 1 } }
-            initial={ { opacity: 0 } }
-            exit={ { opacity: 0 } }
-            transition={ { duration: 0.5 } }
-            className="card">
-                <div className="photo"></div>
-                <div className="rating"><p>{filteredItem.rating}</p></div>
-                <div className="review-count"><p>Based on xx reviews</p></div>
-                <div className="location"><h2>{filteredItem.location}</h2></div>
-                <div className="swiper-wrapper">
-                    <div className="swiper"></div>   
-                    <div className="swiper"></div>             
-                </div>
-        </motion.div>
-    )
+import Review from "./Review";
+
+import { useState } from "react";
+
+function Card({ filteredItem }) {
+  const [activeSlideIndex, setActiveSlideIndex] = useState(0);
+
+  // Create an array of slides (filteredItem and Review) using map()
+  const slides = [
+    {
+      type: 'item',
+      content: (
+        <div>
+          <p>{filteredItem.rating}</p>
+          <p>{filteredItem.location}</p>
+          <p>{filteredItem.address}</p>
+        </div>
+      ),
+    },
+    { type: 'component', content: <Review /> },
+  ];
+
+  return (
+    <motion.div
+      Layout
+      animate={{ opacity: 1 }}
+      initial={{ opacity: 0 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.5 }}
+      className="card"
+    >
+      <Swiper
+        modules={[Navigation, Pagination, A11y]}
+        spaceBetween={0}
+        slidesPerView={1}
+        navigation
+        pagination={{ clickable: true }}
+        onSlideChange={() => console.log("slide change")}
+        onSwiper={(swiper) => console.log(swiper)}
+      >
+        {slides.map((slide, index) => (
+          <SwiperSlide key={index}>
+            {slide.content}
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </motion.div>
+  );
 }
 
-export default Card
+export default Card;
