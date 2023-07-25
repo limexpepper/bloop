739da46638af879ff4f41ec24aa18c19c0accde7
becs <angpohye@gmail.com>
2023-07-25 23:38:31 +0800

    Adding credits to about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,6 +5,8 @@ function About() {
   return (
     <div>
         <h1>About</h1>
+        <h1>Credits</h1>
+        <p>Josh Comeau for sharing the sparkles effect https://www.joshwcomeau.com/react/animated-sparkles-in-react/</p>
     </div>
   )
 }
