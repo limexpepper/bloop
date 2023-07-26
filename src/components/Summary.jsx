a4e012146bc318a7db7a5a5af31173d8e75460a0
becs <angpohye@gmail.com>
2023-07-26 13:46:08 +0800

    formatted Type tags such that first letter is capitalised

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -4,7 +4,16 @@ function Summary({ filteredItem }) {
   return (
     <div>
       <div className="tag-wrapper">
-        <div className="type">{filteredItem.type}</div>
+        <div className="type">{filteredItem.type.charAt(0).toUpperCase() + filteredItem.type.slice(1)}</div>
+      {/*         
+        - The + operator is used to concatenate two strings
+        - filteredItem.type.charAt(0): This part extracts the first character of the filteredItem.type string.
+        - .toUpperCase(): This method is used to convert the first character to uppercase. For example, if the first character is 'a', it becomes 'A'.
+        - filteredItem.type.slice(1): This part extracts the rest of the string starting from the second character. It effectively removes the first character from the original string.
+        
+        As such, the result is from "mall" to "Mall" */}
+        
+        
         <div
           className={
             filteredItem.avgToiletRating === "bronze"
@@ -13,11 +22,13 @@ function Summary({ filteredItem }) {
               ? "rating-tag-silver"
               : filteredItem.avgToiletRating === "gold"
               ? "rating-tag-gold"
-              : "rating-tag-null" // default class name if none of the conditions match
+              : "rating-tag-null"
           }
         >
-          {filteredItem.avgToiletRating}
-        </div> 
+          {filteredItem.avgToiletRating === "NULL"
+            ? "Not yet rated"
+            : filteredItem.avgToiletRating}
+        </div>
       </div>
       <div className="summaryInfo">
         <h1>{filteredItem.location}</h1>
