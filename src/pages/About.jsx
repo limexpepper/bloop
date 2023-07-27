b7cf792cc63c11b6f449092a3e8630ef31a30db4
becs <angpohye@gmail.com>
2023-07-27 17:27:56 +0800

    added resources to about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,6 +7,8 @@ function About() {
         <h1>About</h1>
         <h1>Credits</h1>
         <p>Josh Comeau for sharing the sparkles effect https://www.joshwcomeau.com/react/animated-sparkles-in-react/</p>
+        <p>colours. https://coolors.co/contrast-checker/071218-c0deec , https://color.adobe.com/create/color-wheel , https://hslpicker.com/#f4af01 , https://html-color.codes/brown , https://huemint.com/ , https://colorhunt.co/palettes/popular , https://dribbble.com/shots/popular</p>
+    
     </div>
   )
 }
